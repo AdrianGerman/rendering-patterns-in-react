8b55eb77c2e8d94d2964b46ee5621e654cebc4a4
AdrianGerman <adrian.german1019@gmail.com>
2024-11-13 08:43:04 -0600

    compound components

--- src/components/CompoundComponents/CompoundComponents.tsx
+++ src/components/CompoundComponents/CompoundComponents.tsx
@@ -1,6 +1,12 @@
+import Tab from "./Tab"
+import Tabs from "./Tabs"
+
 export const CompoundComponents = () => {
   return (
-    <>
-    </>
-  );
-};
+    <Tabs>
+      <Tab label="Jirafa">🦒</Tab>
+      <Tab label="Murciélago">🦇</Tab>
+      <Tab label="Delfín">🐬</Tab>
+    </Tabs>
+  )
+}
--- src/components/CompoundComponents/Tab.tsx
+++ src/components/CompoundComponents/Tab.tsx
@@ -1,8 +1,17 @@
-const Tab = () => {
+import { ReactNode } from "react"
+
+interface TabsProps {
+  label: string
+  children: ReactNode
+}
+
+const Tab: React.FC<TabsProps> = ({ label, children }) => {
   return (
     <>
+      <em>{label}</em>
+      <span>{children}</span>
     </>
-  );
-};
+  )
+}
 
-export default Tab;
+export default Tab

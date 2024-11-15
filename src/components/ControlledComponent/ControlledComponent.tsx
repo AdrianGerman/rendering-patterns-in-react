67116ad3c47e1ca8a1a25bb83d82dfc1369fa49f
AdrianGerman <adrian.german1019@gmail.com>
2024-11-15 09:20:34 -0600

    controlled and uncontrolled components

--- src/components/ControlledComponent/ControlledComponent.tsx
+++ src/components/ControlledComponent/ControlledComponent.tsx
@@ -1,6 +1,17 @@
+import { useState } from "react"
+
 export const ControlledInput = () => {
+  const [value, setValue] = useState("")
   return (
     <div>
+      <input
+        type="text"
+        onChange={(e) => setValue(e.target.value)}
+        placeholder="Ingresa el código del cupón. Ej: 100FF"
+      />
+      <p>
+        Cupón de descuento: <b>{value}</b>
+      </p>
     </div>
-  );
-};
+  )
+}
